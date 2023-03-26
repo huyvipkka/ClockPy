339c5e30e2cba96cbe8b202a07adb77c5cd03ac4
Duc Huy <huypoilkj@gmail.com>
2023-03-27 00:48:10 +0700

    v1.01: fix kim giờ ko lệch nhẹ

--- DongHo.py
+++ DongHo.py
@@ -3,27 +3,25 @@ import pygame, datetime, math
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-
 class clock():
-    def __init__(self):
+    def __init__(self) -> None:
         self.__hour = 0
         self.__minute = 0
         self.__second = 0
 
-    def updateTime(self):
+    def updateTime(self) -> None:
         time = datetime.datetime.now()
         self.__hour = time.hour
         self.__minute = time.minute
         self.__second = time.second
     
-    def drawClock(self, surface):
+    def drawClock(self, surface) -> None:
         radius = (300, 250)
         pygame.draw.circle(surface, WHITE, radius, 200, 1)
 
-        tmp = 12
-        for i in range(tmp):
-            pygame.draw.circle(surface, RED,(300 + 190*math.sin((360/tmp)*i*math.pi/180), 
-                                            (250 - 190*math.cos((360/tmp)*i*math.pi/180))), 2)
+        for i in range(12):
+            pygame.draw.circle(surface, RED,(300 + 190*math.sin((360/12)*i*math.pi/180), 
+                                            (250 - 190*math.cos((360/12)*i*math.pi/180))), 2)
 
         pygame.draw.line(surface, WHITE, radius, (300 + 180*math.sin(6*math.pi/180*self.__second), 
                                                  (250 - 180*math.cos(6*math.pi/180*self.__second))), 1)
@@ -32,5 +30,5 @@ class clock():
                                                  (250 - 150*math.cos(6*math.pi/180*self.__minute))), 2)
         if self.__hour >= 12:
             self.__hour -= 12
-        pygame.draw.line(surface, WHITE, radius, (300 + 120*math.sin(30*math.pi/180*self.__hour), 
-                                                 (250 - 120*math.cos(30*math.pi/180*self.__hour))), 3)
+        pygame.draw.line(surface, WHITE, radius, (300 + 120*math.sin(30*math.pi/180*self.__hour + (math.pi/6*self.__minute/60)), 
+                                                 (250 - 120*math.cos(30*math.pi/180*self.__hour + (math.pi/6*self.__minute/60)))), 3)
